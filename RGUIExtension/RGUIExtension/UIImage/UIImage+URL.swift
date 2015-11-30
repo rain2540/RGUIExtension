148dafc53928ebb5061335cac16c8945ca27d394
Smartech <rain2540@foxmail.com>
2015-11-30 13:58:33 +0800

    Add UIImage Extension

---
+++ RGUIExtension/RGUIExtension/UIImage/UIImage+URL.swift
@@ -0,0 +1,26 @@
+//
+//  UIImage+URL.swift
+//  RGUIExtension
+//
+//  Created by RAIN on 15/11/30.
+//  Copyright © 2015年 Smartech. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    /// 通过URL路径获取Image
+    public convenience init?(contentsOfURL URLString: String) {
+        guard let URL = NSURL(string: URLString) else {
+            print("UIImage init Error: URL might not exist")
+            return nil
+        }
+        
+        guard let data = NSData(contentsOfURL: URL) else {
+            print("UIImage init Error: get data fail")
+            return nil
+        }
+        
+        self.init(data: data)
+    }
+}
