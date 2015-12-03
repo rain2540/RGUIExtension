1a99932a71e1e2676a3efb64bce0bc8a1af842e9
Smartech <rain2540@foxmail.com>
2015-12-03 11:04:11 +0800

    Add explains to the extension of UIView

--- RGUIExtension/RGUIExtension/UIView/UIView+DrawTools.swift
+++ RGUIExtension/RGUIExtension/UIView/UIView+DrawTools.swift
@@ -14,15 +14,29 @@ public enum RGLineDirection {
 }
 
 extension UIView {
-    /// 为视图添加边线
-    public func addBorder(width:CGFloat, cornerRadius:CGFloat, color:UIColor) {
+    /**
+     为视图添加边线
+    
+     - parameter width:         边线宽度
+     - parameter cornerRadius:  边线圆角半径
+     - parameter color:         边线颜色
+    */
+    public func addBorder(width: CGFloat, cornerRadius: CGFloat, color: UIColor) {
         self.layer.masksToBounds = true
         self.layer.borderWidth = width
         self.layer.cornerRadius = cornerRadius
         self.layer.borderColor = color.CGColor
     }
     
-    /// 在 view 的指定位置加指定颜色的线
+    /**
+     在 view 的指定位置加指定颜色的线
+     
+     - parameter startPoint: 起始点坐标
+     - parameter length:     长度
+     - parameter width:      宽度
+     - parameter color:      颜色
+     - parameter direction:  方向
+    */
     public func drawLine(startPoint: CGPoint, length: CGFloat, width: CGFloat, color: UIColor, direction: RGLineDirection = .Horizontal) -> UIView {
         let line = UIView()
         line.backgroundColor = color
