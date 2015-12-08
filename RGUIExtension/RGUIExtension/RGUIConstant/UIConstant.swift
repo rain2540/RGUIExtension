0fedcb85977e5a2c73bef693c37120a4cb7cfc5c
Smartech <rain2540@foxmail.com>
2015-12-08 15:35:24 +0800

    Nothing important

--- RGUIExtension/RGUIExtension/RGUIConstant/UIConstant.swift
+++ RGUIExtension/RGUIExtension/RGUIConstant/UIConstant.swift
@@ -8,34 +8,34 @@
 
 import UIKit
 
-/// Status Bar默认高度
+/// Status Bar 默认高度
 public let StatusBarDefaultHeight: CGFloat = 20.0
 
-/// Navigation Bar默认高度
+/// Navigation Bar 默认高度
 public let NavigationBarDefaultHeight: CGFloat = 44.0
 
-/// Top Layout默认高度
+/// Top Layout 默认高度
 public var TopLayoutDefaultHeight: CGFloat {
     return StatusBarDefaultHeight + NavigationBarDefaultHeight
 }
 
-/// Tab Bar默认高度
+/// Tab Bar 默认高度
 public let TabBarDefaultHeight: CGFloat = 49.0
 
-/// Tool Bar默认高度
+/// Tool Bar 默认高度
 public let ToolBarDefaultHeight: CGFloat = 44.0
 
 /// 视图默认间距
 public let ViewDefaultInterval: CGFloat = 8.0
 
-/// Label默认高度
+/// Label 默认高度
 public let LabelDefaultHeight: CGFloat = 21.0
 
-/// Button默认高度
+/// Button 默认高度
 public let ButtonDefaultHeight: CGFloat = 44.0
 
-/// TextField默认高度
+/// TextField 默认高度
 public let TextFieldDefaultHeight: CGFloat = 30.0
 
-/// TableViewCell默认高度
+/// TableViewCell 默认高度
 public let TableViewCellDefaultHeight: CGFloat = 44.0
