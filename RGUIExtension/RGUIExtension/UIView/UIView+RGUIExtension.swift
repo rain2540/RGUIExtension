3c04804711f4e0d8823728927f523e871914ce56
Smartech <rain2540@foxmail.com>
2016-06-22 08:22:30 +0800

    feat (UIView) : add "UIView+RGUIExtension.swift"

---
+++ RGUIExtension/RGUIExtension/UIView/UIView+RGUIExtension.swift
@@ -0,0 +1,65 @@
+//
+//  UIView+DrawTools.swift
+//  RGUIExtension
+//
+//  Created by RAIN on 15/11/30.
+//  Copyright © 2015年 Smartech. All rights reserved.
+//
+
+import UIKit
+
+//  MARK: DrawTools
+/**
+ 绘制线条的方向
+ 
+ - Horizontal: 水平方向
+ - Vertical:   竖直方向
+ */
+public enum RGLineDirection {
+    case Horizontal
+    case Vertical
+}
+
+extension UIView {
+     /**
+     为视图添加边线
+     
+     - parameter width:        边线宽度
+     - parameter cornerRadius: 边线圆角半径, 若无圆角则值为0
+     - parameter color:        边线颜色
+     */
+    public func addBorder(width: CGFloat, cornerRadius: CGFloat, color: UIColor) {
+        self.layer.masksToBounds = true
+        self.layer.borderWidth = width
+        self.layer.cornerRadius = cornerRadius
+        self.layer.borderColor = color.CGColor
+    }
+
+     /**
+     在 view 的指定位置加指定颜色的线
+     
+     - parameter startPoint: 起始点坐标
+     - parameter length:     长度
+     - parameter width:      宽度
+     - parameter color:      颜色
+     - parameter direction:  方向
+     
+     - returns: 按参数条件添加的线
+     */
+    public func drawLine(startPoint: CGPoint, length: CGFloat, width: CGFloat, color: UIColor, direction: RGLineDirection = .Horizontal) -> UIView {
+        let line = UIView()
+        line.backgroundColor = color
+        
+        switch (direction) {
+        case .Horizontal:
+            line.frame = CGRect(x: startPoint.x, y: startPoint.y, width: length, height: width)
+            
+        case .Vertical:
+            line.frame = CGRect(x: startPoint.x, y: startPoint.y, width: width, height: length)
+        }
+        
+        self.addSubview(line)
+        
+        return line
+    }
+}
