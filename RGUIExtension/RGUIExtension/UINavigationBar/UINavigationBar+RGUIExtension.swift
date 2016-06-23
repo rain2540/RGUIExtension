7cc5b3e070325d00d7ae7566e5e2b105af16bfbe
Smartech <rain2540@foxmail.com>
2016-06-23 08:33:58 +0800

    feat (UINavigationBar) : add "UINavigationBar+RGUIExtension.swift"

---
+++ RGUIExtension/RGUIExtension/UINavigationBar/UINavigationBar+RGUIExtension.swift
@@ -0,0 +1,39 @@
+//
+//  UINavigationBar+Colors.swift
+//  RGUIExtension
+//
+//  Created by RAIN on 15/11/30.
+//  Copyright © 2015年 Smartech. All rights reserved.
+//
+
+import UIKit
+
+//  MARK: Colors
+extension UINavigationBar {
+    /**
+     修改 Navigation Bar 的背景颜色
+     
+     - parameter color: 提供给 Navigation Bar 的背景的 tint color
+     */
+    static func barTintColor(color: UIColor) {
+        UINavigationBar.appearance().barTintColor = color
+    }
+    
+    /**
+     修改 Navigation item 和 bar button item 的 tint color
+     
+     - parameter color: 提供给 Navigation item 和 bar button item 的 tint color
+     */
+    static func tintColor(color: UIColor) {
+        UINavigationBar.appearance().tintColor = color
+    }
+    
+    /**
+     修改 Navigation Bar 的文字颜色
+     
+     - parameter color: 提供给 Navigation Bar 上文字的颜色
+     */
+    static func titleTextColor(color: UIColor) {
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: color]
+    }
+}
